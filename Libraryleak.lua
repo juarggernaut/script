f8ebce6fbafe11c14a3321df70d096d6c42aaefa
Dark <100508669+juarggernaut@users.noreply.github.com>
2022-05-09 16:42:44 +0800

    Update Libraryleak.lua

--- Libraryleak.lua
+++ Libraryleak.lua
@@ -1,28 +1,15 @@
 -- // Library
-
 local Library = loadstring(game:HttpGet("here"))()
-
 -- // Window
-
 local Window = Library:Window("RossWare", "Game Name", Enum.KeyCode.V)
-
 -- // Tab
-
 local Tab = Window:Tab("Tab Name")
-
 -- // Button
-
 Tab:Button("Button Text", function()
-
     
-
 end)
-
 -- // Toggle
-
 Tab:Toggle("Toggle Text", function()
-
-    
 
 end)
 
@@ -30,22 +17,12 @@ end)
 
 Tab:Slider("Slider Text", 0, 100, 50, function()
 
-    
-
 end)
-
 -- // Dropdown
-
 Tab:Dropdown("Dropdown Text", {"Food", "Water"}, function()
 
-    
-
 end)
-
 -- // Colorpicker
-
 Tab:Colorpicker("Colorpicker Text", Color3.fromRGB(255, 255, 255), function()
-
-    
 
 end)
